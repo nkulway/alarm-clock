9248c009b12fac1a46958096859b08484b83c74a
Nick <nkulway@gmail.com>
2023-07-17 21:34:39 -0400

    include weather in message

--- light_sensor.py
+++ light_sensor.py
@@ -1,37 +1,15 @@
-import sys
 import os
+import sys
 import openai
 import RPi.GPIO as GPIO
 import time
 from elevenlabs import generate, play, set_api_key, clone
 from dotenv import load_dotenv
+from weather import weather
 
 load_dotenv()
-set_api_key(os.environ.get('ELEVEN_LABS_API_KEY'))
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Give a wakeup greeting no longer than 20 words like a pirate."}
-  ]
-)
-
-# To be used with paid tier
-
-# voice = clone(
-#     name="Gandalf",
-#     description="A wizard who proves himself to possess the leadership traits of Impartiality, experience, kindness and affection for all others, he commands respect, he is wise and thoughtful and he understands the need for sacrifice to achieve the ultimate victory for Middle Earth.",
-#     files=["./samples/1a-a-wizard-is-never-late.mp3", "./samples/1c-im-trying-to-help-you.mp3"]
-# )
-
-audio = generate(
-#   text="Ahoy, me hearties! Rise and shine, ye scurvy landlubbers! 'Tis a new day brimming with adventure!", - Sample text
-  text=completion.choices[0].message.content,
-  voice="Sam",
-  model="eleven_monolingual_v1"
-)
-
+set_api_key(os.environ.get("ELEVEN_LABS_API_KEY"))
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -41,13 +19,13 @@ while isNight:
     GPIO.setup(resistorPin, GPIO.OUT)
     GPIO.output(resistorPin, GPIO.LOW)
     time.sleep(0.1)
-    
+
     GPIO.setup(resistorPin, GPIO.IN)
     currentTime = time.time()
     diff = 0
-    
-    while(GPIO.input(resistorPin) == GPIO.LOW):
-        diff  = time.time() - currentTime
+
+    while GPIO.input(resistorPin) == GPIO.LOW:
+        diff = time.time() - currentTime
 
     sensorReading = diff * 1000
 
@@ -59,10 +37,39 @@ while isNight:
     elif sensorReading < 30:
         # trigger generative ai voice
         print("TIME TO WAKE UP")
+        res = weather()
+        print(res["weather"][0]["description"])
+        weather = res["weather"][0]["description"]
+        temp = res['main']['temp']
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Give a wakeup greeting mentioning this value as the temperature {temp} and this weather description {weather} no longer than 30 words like a pirate.",
+                }
+            ],
+        )
+
+        audio = generate(
+            text=completion.choices[0].message.content,
+            voice="Sam",
+            model="eleven_monolingual_v1",
+        )
+
         print(completion.choices[0].message.content)
 
         play(audio)
         print(sys.path)
         isNight = False
-    
+
     time.sleep(1)
+
+
+# To be used with paid tier
+
+# voice = clone(
+#     name="Gandalf",
+#     description="A wizard who proves himself to possess the leadership traits of Impartiality, experience, kindness and affection for all others, he commands respect, he is wise and thoughtful and he understands the need for sacrifice to achieve the ultimate victory for Middle Earth.",
+#     files=["./samples/1a-a-wizard-is-never-late.mp3", "./samples/1c-im-trying-to-help-you.mp3"]
+# )
