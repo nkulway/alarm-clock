3d80d0fd2a255ba3ad6e8e2273fc4d00a0e08ce0
Nick <nkulway@gmail.com>
2025-01-21 21:15:39 -0500

    update location to ATL

--- resources/Weather.py
+++ resources/Weather.py
@@ -9,6 +9,6 @@ api_key = os.environ.get("WEATHER_API_KEY")
 class Weather:
     def weather():
         res = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat=41.40&lon=2.17&appid={api_key}&units=imperial"
+            f"https://api.openweathermap.org/data/2.5/weather?lat=33.7490&lon=-84.3880&appid={api_key}&units=imperial"
         )
         return res.json()
